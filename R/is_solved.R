2ec06ba51b216cc4bb4357d71fe4bacb7f87dc94
myriam <myriam.1232003@gmail.com>
2026-04-07 17:09:33 +0200

    Mise à jour de la fonction is_solved

--- R/is_solved.R
+++ R/is_solved.R
@@ -1,3 +1,17 @@
 is_solved <- function(game) {
-  return(FALSE)
+  clues <- game$clues
+  segments <- game$segments
+  n <- game$n
+
+  for (i in 1:n) {
+    for (j in 1:n) {
+      val <- clues[i,j]
+      if (!is.na(val)) {
+        count <- segments$horiz[i,j] + segments$horiz[i+1,j] +
+          segments$vert[i,j] + segments$vert[i,j+1]
+        if (count != val) return(FALSE)
+      }
+    }
+  }
+  return(TRUE)
 }
