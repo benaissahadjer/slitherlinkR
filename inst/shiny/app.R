1be694c5a192844b118159481f48cf13ba1b3ad6
myriam <myriam.1232003@gmail.com>
2026-04-09 10:49:36 +0200

    Création des segments horizontaux et verticaux

--- inst/shiny/app.R
+++ inst/shiny/app.R
@@ -89,7 +89,7 @@ ui <- fluidPage(
     column(
       8,
       div(class = "game-box",
-          plotOutput("grid", height = "500px")
+          plotOutput("gridPlot", click = "plot_click")
       ),
       div(class = "status-box",
           textOutput("status")
@@ -100,12 +100,77 @@ ui <- fluidPage(
 
 server <- function(input, output, session) {
 
+  segments <- reactiveValues(
+    horiz = NULL,
+    vert = NULL
+  )
+
+  observeEvent(input$plot_click, {
+
+    req(game())
+    req(segments$horiz)
+
+    x <- input$plot_click$x
+    y <- input$plot_click$y
+    n <- game()$n
+
+    # position dans la grille
+    j <- floor(x) + 1
+    i <- floor(y) + 1
+
+    # distance aux bords de la case
+    dx <- x - floor(x)
+    dy <- y - floor(y)
+
+    # distance aux 4 côtés
+    dist_top <- dy
+    dist_bottom <- 1 - dy
+    dist_left <- dx
+    dist_right <- 1 - dx
+
+    min_dist <- min(dist_top, dist_bottom, dist_left, dist_right)
+
+    # haut
+    if (min_dist == dist_top) {
+      if (i >= 1 && i <= n+1 && j >= 1 && j <= n) {
+        segments$horiz[i, j] <- 1 - segments$horiz[i, j]
+      }
+    }
+
+    # bas
+    else if (min_dist == dist_bottom) {
+      if (i+1 >= 1 && i+1 <= n+1 && j >= 1 && j <= n) {
+        segments$horiz[i+1, j] <- 1 - segments$horiz[i+1, j]
+      }
+    }
+
+    # gauche
+    else if (min_dist == dist_left) {
+      if (i >= 1 && i <= n && j >= 1 && j <= n+1) {
+        segments$vert[i, j] <- 1 - segments$vert[i, j]
+      }
+    }
+
+    # droite
+    else if (min_dist == dist_right) {
+      if (i >= 1 && i <= n && j+1 >= 1 && j+1 <= n+1) {
+        segments$vert[i, j+1] <- 1 - segments$vert[i, j+1]
+      }
+    }
+
+  })
+
   game <- reactiveVal(NULL)
+
+
+
   status_text <- reactiveVal("Choisissez une difficulté puis lancez une nouvelle partie.")
 
   observeEvent(input$new, {
     n <- if (input$niveau == "Facile") 5 else if (input$niveau == "Moyen") 7 else 10
     game(create_game(n))
+    segments$horiz <- matrix(0, n+1, n)
+    segments$vert  <- matrix(0, n, n+1)
     status_text("Jeu en cours")
   })
 
@@ -113,19 +178,27 @@ server <- function(input, output, session) {
     req(game())
     n <- game()$n
     game(create_game(n))
+    segments$horiz <- matrix(0, n+1, n)
+    segments$vert  <- matrix(0, n, n+1)
     status_text("Grille réinitialisée")
   })
 
   observeEvent(input$check, {
     req(game())
-    if (is_solved(game())) {
+    g <- game()
+
+    g$segments <- list(
+      horiz = segments$horiz,
+      vert = segments$vert
+    )
+    if (is_solved(g)) {
       status_text("Bravo, la solution est correcte !")
     } else {
       status_text("La solution n'est pas encore correcte.")
     }
   })
 
-  output$grid <- renderPlot({
+  output$gridPlot <- renderPlot({
     req(game())
     n <- game()$n
 
@@ -152,7 +225,31 @@ server <- function(input, output, session) {
         }
       }
     }
+    # afficher segments horizontaux
+    if (!is.null(segments$horiz)) {
+      for (i in 1:nrow(segments$horiz)) {
+        for (j in 1:ncol(segments$horiz)) {
+          if (segments$horiz[i,j] == 1) {
+            segments(j-1, i-1, j, i-1, lwd = 3)
+          }
+        }
+      }
+    }
+
+    # afficher segments verticaux
+    if (!is.null(segments$vert)) {
+      for (i in 1:nrow(segments$vert)) {
+        for (j in 1:ncol(segments$vert)) {
+          if (segments$vert[i,j] == 1) {
+            segments(j-1, i-1, j-1, i, lwd = 3)
+          }
+        }
+      }
+    }
+
   })
+
+
 
   output$status <- renderText({
     status_text()
